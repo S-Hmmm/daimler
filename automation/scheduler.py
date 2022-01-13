ee07ca2e21cd563bca24e2754bd88543e776315d
sunny <morningtest@126.com>
2022-01-13 14:55:10 +0800

    modify scheduler

--- automation/scheduler.py
+++ automation/scheduler.py
@@ -1,12 +1,12 @@
 from datetime import datetime as dt
 
 from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
-# from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 from apscheduler.jobstores.mongodb import MongoDBJobStore, MongoClient
 from apscheduler.jobstores.memory import MemoryJobStore
-# from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.triggers.interval import IntervalTrigger
 
 
 def time_work():
@@ -36,19 +36,32 @@ executors = {
 }
 job_defaults = {
     'coalesce': False,
-    'max_instances': 3
+    'max_instances': 5
 }
 
+scheduler = BackgroundScheduler(jobstores=job_stores,
+                                executors=executors,
+                                job_defaults=job_defaults,
+                                timezone='Asia/Shanghai',
+                                )
 
 if __name__ == '__main__':
-    scheduler = BlockingScheduler(jobstores=job_stores, executors=executors, job_defaults=job_defaults,
-                                  timezone='Asia/Shanghai')
-    scheduler.add_job(time_work, 'interval', seconds=10, id='time_work', jobstore='mongo', replace_existing=True)
-    # scheduler.add_job(time_work, 'date', run_date='2021-12-17 16:57:50')
-    # scheduler.add_job(time_work, 'cron', hour=10, minute='09-10', id='time_work1')
+    print('JOBS', scheduler.get_jobs())
+    if scheduler.get_jobs(jobstore='default'):
+        scheduler.remove_all_jobs(jobstore='default')
+
+    interval_trigger = IntervalTrigger(seconds=10, timezone='Asia/Shanghai')
+    scheduler.add_job(time_work, trigger=interval_trigger, id='time_work',
+                      jobstore='default', replace_existing=True)
+    scheduler.add_job(time_work, 'date', run_date='2022-01-13 14:01:10', id='ot')
+    scheduler.add_job(time_work, 'cron', hour=13, minute='55', id='time_work',
+                      jobstore='mongo', replace_existing=True)
+
     scheduler.add_listener(listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
-    scheduler.pause_job(job_id='time_work')
-    scheduler.print_jobs()
+    # scheduler.pause_job(job_id='time_work')
+    # scheduler.print_jobs()
     scheduler.start()
     # scheduler.shutdown(wait=False)
-    print('abc')
+    while 1:
+        if not scheduler.get_jobs(jobstore='default'):
+            break
